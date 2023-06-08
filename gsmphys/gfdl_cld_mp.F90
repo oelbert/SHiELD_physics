b964d0ee1b4e5fd2a4c3bcff3de444abab28ff81
Oliver Elbert <Oliver.Elbert@noaa.gov>
2023-06-08 15:45:18 -0400

    awevjav'

--- gsmphys/gfdl_cld_mp.F90
+++ gsmphys/gfdl_cld_mp.F90
@@ -2634,7 +2634,7 @@ subroutine mp_fast (ks, ke, tz, qv, ql, qr, qi, qs, qg, dtm, dp, den, &
     real, intent (in), dimension (ks:ke) :: dp, den
     
     real, intent (inout), dimension (ks:ke) :: qv, ql, qr, qi, qs, qg, ccn, cin
-    !$ser verbatim real, intent (inout), dimension (ks:ke) ::  szs_qsi, szs_dqdt, szs_pidep0, szs_pidep, szs_qi_crt, szs_sink1, szs_sink2,&
+    !$ser verbatim real, intent (inout), dimension (ks:ke) ::  szs_qsi, szs_dqdt, szs_pidep0, szs_pidep, szs_qi_crt, szs_sink1, szs_sink2
     
     real (kind = r8), intent (inout), dimension (ks:ke) :: tz
     
