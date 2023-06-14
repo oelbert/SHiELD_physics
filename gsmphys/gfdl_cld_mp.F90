5029a5c15651f6b18942af4940246f164c0e75c6
Oliver Elbert <Oliver.Elbert@noaa.gov>
2023-06-14 10:57:41 -0400

    serialize psdep_pssub

--- gsmphys/gfdl_cld_mp.F90
+++ gsmphys/gfdl_cld_mp.F90
@@ -5159,52 +5159,9 @@ subroutine subgrid_z_proc (ks, ke, den, denfac, dts, rh_adj, tz, qv, ql, qr, &
     ! cloud water condensation and evaporation
     ! -----------------------------------------------------------------------
 
-    !$ser verbatim if (nn .eq. 1) then
-        !$ser verbatim szs_pt = tz
-        !$ser verbatim szs_qv = qv
-        !$ser verbatim szs_ql = ql
-        !$ser verbatim szs_qr = qr
-        !$ser verbatim szs_qi = qi
-        !$ser verbatim szs_qs = qs
-        !$ser verbatim szs_qg = qg
-        !$ser verbatim szs_ccn = ccn
-        !$ser verbatim szs_cin = cin
-        !$ser verbatim szs_cond = cond
-        !$ser verbatim szs_dep = dep
-        !$ser verbatim szs_reevap = reevap
-        !$ser verbatim szs_sub = sub
-        !$ser verbatim szs_te = te8
-        !$ser verbatim szs_lcpk = lcpk
-        !$ser verbatim szs_icpk = icpk
-        !$ser verbatim szs_tcpk = tcpk
-        !$ser verbatim szs_tcp3 = tcp3
-        !$ser verbatim szs_cvm = cvm
-    !$ser verbatim endif
-
     call pcond_pevap (ks, ke, dts, qv, ql, qr, qi, qs, qg, tz, dp, cvm, te8, den, &
-!$ser verbatim nn, szs_qsi, szs_dqidt, szs_qsw, szs_dqwdt,&
+!$ser verbatim nn, buf_qsi, buf_dqidt, buf_qsw, buf_dqwdt,&
         lcpk, icpk, tcpk, tcp3, cond, reevap)
-
-        !$ser verbatim if (nn .eq. 1) then
-            !$ser verbatim szs_pto = tz
-            !$ser verbatim szs_qvo = qv
-            !$ser verbatim szs_qlo = ql
-            !$ser verbatim szs_qro = qr
-            !$ser verbatim szs_qio = qi
-            !$ser verbatim szs_qso = qs
-            !$ser verbatim szs_qgo = qg
-            !$ser verbatim szs_ccno = ccn
-            !$ser verbatim szs_cino = cin
-            !$ser verbatim szs_condo = cond
-            !$ser verbatim szs_depo = dep
-            !$ser verbatim szs_reevapo = reevap
-            !$ser verbatim szs_subo = sub
-            !$ser verbatim szs_lcpko = lcpk
-            !$ser verbatim szs_icpko = icpk
-            !$ser verbatim szs_tcpko = tcpk
-            !$ser verbatim szs_tcp3o = tcp3
-            !$ser verbatim szs_cvmo = cvm
-        !$ser verbatim endif
     
     if (.not. do_warm_rain_mp) then
         
@@ -5240,10 +5197,53 @@ subroutine subgrid_z_proc (ks, ke, den, denfac, dts, rh_adj, tz, qv, ql, qr, &
         ! snow deposition and sublimation
         ! -----------------------------------------------------------------------
         
+    !$ser verbatim if (nn .eq. 1) then
+        !$ser verbatim szs_pt = tz
+        !$ser verbatim szs_qv = qv
+        !$ser verbatim szs_ql = ql
+        !$ser verbatim szs_qr = qr
+        !$ser verbatim szs_qi = qi
+        !$ser verbatim szs_qs = qs
+        !$ser verbatim szs_qg = qg
+        !$ser verbatim szs_ccn = ccn
+        !$ser verbatim szs_cin = cin
+        !$ser verbatim szs_cond = cond
+        !$ser verbatim szs_dep = dep
+        !$ser verbatim szs_reevap = reevap
+        !$ser verbatim szs_sub = sub
+        !$ser verbatim szs_te = te8
+        !$ser verbatim szs_lcpk = lcpk
+        !$ser verbatim szs_icpk = icpk
+        !$ser verbatim szs_tcpk = tcpk
+        !$ser verbatim szs_tcp3 = tcp3
+        !$ser verbatim szs_cvm = cvm
+    !$ser verbatim endif
+
         call psdep_pssub (ks, ke, dts, qv, ql, qr, qi, qs, qg, tz, dp, cvm, te8, den, &
-!$ser verbatim nn, buf_qsi, buf_dqidt, buf_qsw, buf_dqwdt,&
+!$ser verbatim nn, szs_qsi, szs_dqidt, szs_qsw, szs_dqwdt,&
             denfac, lcpk, icpk, tcpk, tcp3, dep, sub)
         
+        !$ser verbatim if (nn .eq. 1) then
+            !$ser verbatim szs_pto = tz
+            !$ser verbatim szs_qvo = qv
+            !$ser verbatim szs_qlo = ql
+            !$ser verbatim szs_qro = qr
+            !$ser verbatim szs_qio = qi
+            !$ser verbatim szs_qso = qs
+            !$ser verbatim szs_qgo = qg
+            !$ser verbatim szs_ccno = ccn
+            !$ser verbatim szs_cino = cin
+            !$ser verbatim szs_condo = cond
+            !$ser verbatim szs_depo = dep
+            !$ser verbatim szs_reevapo = reevap
+            !$ser verbatim szs_subo = sub
+            !$ser verbatim szs_lcpko = lcpk
+            !$ser verbatim szs_icpko = icpk
+            !$ser verbatim szs_tcpko = tcpk
+            !$ser verbatim szs_tcp3o = tcp3
+            !$ser verbatim szs_cvmo = cvm
+        !$ser verbatim endif
+
         ! -----------------------------------------------------------------------
         ! graupel deposition and sublimation
         ! -----------------------------------------------------------------------
