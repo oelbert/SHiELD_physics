8bd73f07138af47b1281d9fb37757f09c95959c3
Oliver Elbert <oliver.elbert36@gmail.com>
2024-08-16 11:16:32 -0400

    rstjghnsprt

--- GFS_layer/GFS_physics_driver.F90
+++ GFS_layer/GFS_physics_driver.F90
@@ -435,7 +435,7 @@ module module_physics_driver
 
       !--- LOGICAL VARIABLES
       logical :: lprnt, revap, do_awdd, trans_aero
-      !$ser verbatim logical :: mp_consv_te, mp_last_step,
+      !$ser verbatim logical :: mp_consv_te, mp_last_step
       !$ser verbatim logical :: redrag, do_z0_moon, do_z0_hwrf15, do_z0_hwrf17, do_dk_hb19
       !$ser verbatim logical :: do_z0_hwrf17_hwonly, mom4ice, lheatstrg, dspheat, cap_k0_land
       
